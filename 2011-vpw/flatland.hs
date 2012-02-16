ee7323c34da83362589668b9c41e2e2e11c58dfa
Jasper Van der Jeugt <m@jaspervdj.be>
2012-02-16 01:31:42 +0100

    flatland, still doesn't work...

--- 2011-vpw/flatland.hs
+++ 2011-vpw/flatland.hs
@@ -1,4 +1,8 @@
+import Control.Applicative ((<$>))
+import Control.Monad (forM_, replicateM, replicateM_)
+import Data.List (intercalate)
 
+import Debug.Trace
 
 type Rectangular = (Double, Double)
 
@@ -12,7 +16,24 @@ toPolar (x, y) = (sqrt (x * x + y * y), atan (y / x))
 fromPolar :: Polar -> Rectangular
 fromPolar (r, q) = (r * cos q, r * sin q)
 
+toDegrees :: Double -> Double
+toDegrees r = r * 180 / pi
+
+fromDegrees :: Double -> Double
+fromDegrees d = d * pi / 180
+
 data Bomb = Bomb Rectangular Double Double Double  -- (x, y), a, b, r
+
+parseBomb :: String -> Bomb
+parseBomb str = case map read (words str) of
+    [x, y, a, b, r] -> Bomb (x, y) (fromDegrees a) (fromDegrees b) r
+    _               -> error "parseBomb"
+
+bombLines :: Bomb -> (Line, Line)
+bombLines (Bomb (x, y) a b r) = (((x, y), end1), ((x, y), end2))
+  where
+    end1 = let (dx, dy) = fromPolar (r, a)     in (x + dx, y + dy)
+    end2 = let (dx, dy) = fromPolar (r, a + b) in (x + dx, y + dy)
 
 inRadius :: Bomb -> Rectangular -> Bool
 inRadius (Bomb (bx, by) ba bb br) (x, y)
@@ -23,12 +44,12 @@ inRadius (Bomb (bx, by) ba bb br) (x, y)
 
 lineIntersection :: Line -> Line -> Bool
 lineIntersection ((x1, y1), (x2, y2)) ((x3, y3), (x4, y4))
-    | d == 0                       = False
-    | x < minimum [x1, x2, x3, x4] = False
-    | x > maximum [x1, x2, x3, x4] = False
-    | y < minimum [y1, y2, y3, y4] = False
-    | y > maximum [y1, y2, y3, y4] = False
-    | otherwise                    = True
+    | d == 0                         = False
+    | x < min x1 x2 || x > max x2 x2 = False
+    | x < min x3 x4 || x > max x3 x4 = False
+    | y < min y1 y2 || y > max y1 y2 = False
+    | y < min y3 y4 || y > max y3 y4 = False
+    | otherwise                      = True
   where
     d    = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4)
     pre  = x1 * y2 - y1 * x2
@@ -38,8 +59,13 @@ lineIntersection ((x1, y1), (x2, y2)) ((x3, y3), (x4, y4))
 
 data Soldier = Soldier Rectangular Double Double  -- (x, y) d h
 
-soldierCorners :: Soldier -> [Rectangular]
-soldierCorners (Soldier (x, y) d h) =
+parseSoldier :: String -> Soldier
+parseSoldier str = case map read (words str) of
+    [x, y, d, h] -> Soldier (x, y) d h
+    _            -> error "parseSoldier"
+
+soldierPoints :: Soldier -> [Rectangular]
+soldierPoints (Soldier (x, y) d h) =
     [ (x + dx, y + dy)
     | i <- [0 .. h - 1]
     , let (dx, dy) = fromPolar (d, i * step)
@@ -48,7 +74,61 @@ soldierCorners (Soldier (x, y) d h) =
     step = 2 * pi / h
 
 soldierLines :: Soldier -> [Line]
-soldierLines soldier = zip corners corners'
+soldierLines soldier = zip points points'
   where
-    corners  = soldierCorners soldier
-    corners' = tail corners ++ [head corners]
+    points  = soldierPoints soldier
+    points' = tail points ++ [head points]
+
+kills :: [Bomb] -> Soldier -> Bool
+kills bombs soldier =
+    killsByEdge bombs soldier ||
+    trace "death by splitting" (any (\b -> killsBySplitting b soldier) bombs)
+
+killsByEdge :: [Bomb] -> Soldier -> Bool
+killsByEdge bombs soldier =
+    any (== (True, True)) $
+    map snd $
+    foldl step (zip (soldierLines soldier) $ repeat (False, False)) bombs
+  where
+    step ls bomb                    = map (flip step' bomb) ls
+    step' ((p1, p2), (h1, h2)) bomb =
+        ((p1, p2), (h1 || inRadius bomb p1, h2 || inRadius bomb p2))
+      
+killsBySplitting :: Bomb -> Soldier -> Bool
+killsBySplitting bomb soldier =
+    let (l1, l2) = bombLines bomb
+    in all killsBySplitting' [l1, l2]
+  where
+    killsBySplitting' l = any (lineIntersection l) (soldierLines soldier)
+
+plotBomb :: Bomb -> String
+plotBomb bomb =
+    let ((p1, p2), (_, p3)) = bombLines bomb
+    in plotPoints "red" [p1, p2, p3]
+
+plotSoldier :: Soldier -> String
+plotSoldier = plotPoints "blue" . soldierPoints
+
+plotScene :: [Bomb] -> [Soldier] -> String
+plotScene bombs soldiers = unlines $
+    ["plot(c(), xlim=c(-30, 30), ylim=c(-30, 30))"] ++
+    map plotBomb bombs ++
+    map plotSoldier soldiers
+
+plotPoints :: String -> [Rectangular] -> String
+plotPoints col ps =
+    "points(c(" ++ list xs ++ "), c(" ++ list ys ++ "), col='" ++ col ++ "')"
+  where
+    list     = intercalate ", " . map show
+    (xs, ys) = unzip ps
+
+main :: IO ()
+main = do
+    cases <- readLn
+    replicateM_ cases $ do
+        [numBombs, numSoldiers] <- map read . words <$> getLine
+        bombs    <- map parseBomb    <$> replicateM numBombs getLine
+        soldiers <- map parseSoldier <$> replicateM numSoldiers getLine
+        putStr $ plotScene bombs soldiers
+        forM_ soldiers $ \soldier -> putStrLn $
+            if kills bombs soldier then "dood" else "levend"
