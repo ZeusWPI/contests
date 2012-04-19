1d027131f2c9a905035a26f2d95b4b693e777bb1
Jasper Van der Jeugt <m@jaspervdj.be>
2012-04-19 12:25:10 +0200

    Clean up morse problem

--- 2012-vpw/morse.hs
+++ 2012-vpw/morse.hs
@@ -9,25 +9,21 @@ type Morse a = State (Map (Char, Integer) Integer) a
 enc :: Char -> String
 enc '.' = "-."
 enc '-' = "...-"
+enc _   = ""
 
 solve :: Integer -> Char -> Morse Integer
 solve 1 c = return $ fromIntegral $ length (enc c)
 solve n c = do
-    map' <- get
-    case M.lookup (c, n) map' of
+    cache <- get
+    case M.lookup (c, n) cache of
         Just i -> return i
         _      -> do
-            let enc' = enc c
-            ls <- mapM (solve (n - 1)) enc'
-            let sum' = sum ls
+            sum' <- sum <$> mapM (solve (n - 1)) (enc c)
             modify (M.insert (c, n) sum')
             return sum'
 
-solve' :: Integer -> String -> Morse Integer
-solve' n str = sum <$> mapM (solve n) str
-
 runSolve :: Integer -> String -> Integer
-runSolve n str = evalState (solve' n str) M.empty
+runSolve n str = evalState (sum <$> mapM (solve n) str) M.empty
 
 main :: IO ()
 main = do
