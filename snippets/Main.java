405638bde818997d2a5bdf815d413b60e497e53f
Felix Van der Jeugt <felix.vanderjeugt@ugent.be>
2015-02-17 11:11:26 +0100

    Update Main.java

--- snippets/Main.java
+++ snippets/Main.java
@@ -5,7 +5,7 @@ import java.io.*;
  */
 public class Main {
     public static void main(String[] args) throws IOException {
-       BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(reader.readLine());
         for(int i = 0; i < n; i++) {
             boolean result = run(reader);
